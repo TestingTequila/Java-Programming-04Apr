d5ee07c56f0e21abcf270b376f6ec53487a01af2
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-04-15 08:07:15 +0530

    JDK/Java Basics/Variables/Data Types

---
+++ src/com/janbaskdemo/variables/VariableBasics.java
@@ -0,0 +1,18 @@
+package com.janbaskdemo.variables;
+
+public class VariableBasics {
+
+    public static void main(String[] args) {
+
+        byte x = 10;
+        double y=12.34;
+
+    }
+
+}
+
+
+
+//Byte, short, int, long, float, double, char, boolean
+
+
